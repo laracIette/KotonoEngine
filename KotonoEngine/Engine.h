724c8f3e9d8d645f7d0706a697e3369a4f9f4210
laracIette <nicosavlar@gmail.com>
2025-04-24 19:30:20 +0200

    refac: engine and framework inline classes forwarding

--- KotonoEngine/Engine.h
+++ KotonoEngine/Engine.h
@@ -1,16 +1,13 @@
 #pragma once
-class KTime;
-class KObjectManager;
-class KVisualizer;
 class KEngine
 {
 public:
 	void Init();
 	void Cleanup();
 
-	KTime& GetTime();
-	KObjectManager& GetObjectManager();
-	KVisualizer& GetVisualizer();
+	class KTime& GetTime();
+	class KObjectManager& GetObjectManager();
+	class KVisualizer& GetVisualizer();
 };
 
 extern KEngine Engine;
--- KotonoFramework/include/kotono_framework/Framework.h
+++ KotonoFramework/include/kotono_framework/Framework.h
@@ -1,28 +1,19 @@
 #pragma once
-class KtPath;
-class KtWindow;
-class KtContext;
-class KtRenderer;
-class KtAudioManager;
-class KtInputManager;
-class KtModelManager;
-class KtShaderManager;
-class KtImageTextureManager;
 class KtFramework final
 {
 public:
 	void Init();
 	void Cleanup();
 
-	KtPath& GetPath();
-	KtWindow& GetWindow();
-	KtContext& GetContext();
-	KtRenderer& GetRenderer();
-	KtAudioManager& GetAudioManager();
-	KtInputManager& GetInputManager();
-	KtModelManager& GetModelManager();
-	KtShaderManager& GetShaderManager();
-	KtImageTextureManager& GetImageTextureManager();
+	class KtPath& GetPath();
+	class KtWindow& GetWindow();
+	class KtContext& GetContext();
+	class KtRenderer& GetRenderer();
+	class KtAudioManager& GetAudioManager();
+	class KtInputManager& GetInputManager();
+	class KtModelManager& GetModelManager();
+	class KtShaderManager& GetShaderManager();
+	class KtImageTextureManager& GetImageTextureManager();
 };
 
 extern KtFramework Framework;
