6b92ea2c69e20d2e28c19de38a88618efe1c163c
laracIette <nicosavlar@gmail.com>
2025-05-04 15:53:36 +0200

    refac: removed collection test code

--- KotonoEngine/main.cpp
+++ KotonoEngine/main.cpp
@@ -6,43 +6,8 @@
 #include "Time.h"
 #include "ObjectManager.h"
 
-
-#include <kotono_framework/Collection.h>
-
-
 int main()
 {
-    std::vector<int*> vec1 = { new int(0), new int(1), new int(2), new int(3), new int(4), new int(5), new int(6), new int(7), new int(8), new int(9) };
-    auto collection1 = KtCollection<int*>(vec1);
-    collection1.AddFilter([](int* item) { return *item % 2 == 0; });
-    collection1.AddFilter([](int* item) { return *item > 5; });
-
-    for (const auto* item : collection1)
-    {
-        KT_DEBUG_LOG("%u", *item);
-    }
-
-    std::vector<int> vec2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    auto collection2 = KtCollection<int>(vec2);
-    collection2.AddFilter([](int item) { return item % 2 == 0; });
-    collection2.AddFilter([](int item) { return item > 5; });
-    
-    for (const auto item : collection2)
-    {
-        KT_DEBUG_LOG("%u", item);
-    }
-
-    auto collection3 = KtCollection<int>({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-    collection3.AddFilter([](int item) { return item % 2 == 0; });
-    collection3.AddFilter([](int item) { return item > 5; });
-    
-    for (const auto item : collection3)
-    {
-        KT_DEBUG_LOG("%u", item);
-    }
-
-    return 0;
-
     Framework.Init();
     Engine.Init();
 
