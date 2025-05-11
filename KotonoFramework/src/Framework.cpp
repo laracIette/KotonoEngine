a68f226bfcdcb66eb1aa6ac20b7bb1f309bb10f2
laracIette <nicosavlar@gmail.com>
2025-05-11 14:05:03 +0200

    feat: compile only update shaders

--- KotonoFramework/src/Framework.cpp
+++ KotonoFramework/src/Framework.cpp
@@ -8,11 +8,12 @@
 #include "ModelManager.h"
 #include "ShaderManager.h"
 #include "ImageTextureManager.h"
-#include "KtSpvCompiler.h"
+#include "SpvCompiler.h"
 
 KtFramework Framework;
 
 static KtPath Path;
+static KtSpvCompiler SpvCompiler;
 static KtWindow Window;
 static KtContext Context;
 static KtRenderer Renderer;
@@ -25,7 +26,7 @@ static KtImageTextureManager ImageTextureManager;
 void KtFramework::Init()
 {
     Path.Init();
-    KtSpvCompiler().CompileAll();
+    SpvCompiler.CompileUpdated(); // todo: move to shader manager ?
     Window.Init();
     Context.Init();
     Renderer.Init();
---
+++ KotonoFramework/src/SpvCompiler.cpp
@@ -0,0 +1,86 @@
+#include "SpvCompiler.h"
+#include "Framework.h"
+#include "Path.h"
+#include "log.h"
+#include <nlohmann/json.hpp>
+#include "Serializer.h"
+#include <format>
+
+void KtSpvCompiler::CompileUpdated() const
+{
+    const auto path = Framework.GetPath().GetFrameworkPath() / "shaders";
+    nlohmann::json json;
+    KtSerializer serializer;
+
+    serializer.ReadData(path / "shaders.ktregistry", json);
+    for (const auto& directory : { "vert", "frag" })
+    {
+        for (const auto& entry : std::filesystem::directory_iterator(path / directory))
+        {
+            // not a glsl file
+            if (entry.path().extension() != std::format(".{}", directory))
+            {
+                continue;
+            }
+
+            const auto ftime = entry.last_write_time();
+            const auto formattedTime = std::format("{0:%F}-{0:%T}", ftime);
+
+            bool isInList = false;
+            for (auto& shader : json["shaders"])
+            {
+                if (shader["path"] != entry.path().string())
+                {
+                    continue;
+                }
+
+                if (shader["modified"] != formattedTime)
+                {
+                    shader["modified"] = formattedTime;
+                    Compile(entry.path());
+                }
+
+                isInList = true;
+                break;
+            }
+
+            if (!isInList)
+            {
+                nlohmann::json shader;
+                shader["path"] = entry.path().string();
+                shader["modified"] = formattedTime;
+                json["shaders"].push_back(shader);
+                Compile(entry.path());
+            }
+        }
+    }
+
+    serializer.WriteData(path / "shaders.ktregistry", json);
+}
+
+void KtSpvCompiler::CompileAll() const
+{
+    KT_DEBUG_LOG("compiling all spirv shaders");
+
+    const auto path = Framework.GetPath().GetFrameworkPath() / "shaders";
+    for (const auto& directory : { "vert", "frag" })
+    {
+        for (const auto& entry : std::filesystem::directory_iterator(path / directory))
+        {
+            if (entry.path().extension() == std::format(".{}", directory))
+            {
+                Compile(entry.path());
+            }
+        }
+    }
+
+    KT_DEBUG_LOG("compiled all spirv shaders");
+}
+
+void KtSpvCompiler::Compile(const std::filesystem::path& path) const
+{
+    KT_DEBUG_LOG("%s", path.string().c_str());
+    // must have vulkan bin in environment variables path
+    const auto command = std::format("glslc \"{}\" -o \"{}\".spv", path.string(), path.string());
+    std::system(command.c_str());
+}
